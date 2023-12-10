96612bc223f583f7a78f11157efd699f6fbc181a
Akram Dghais <115045927+1kram9dghs@users.noreply.github.com>
2023-12-10 20:09:17 +0100

    Add files via upload

---
+++ src/main/java/com/akram/livres/repos/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.akram.livres.repos;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.akram.livres.entities.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByRole(String role);
+}
---
+++ src/main/java/com/akram/livres/restController/UserRestController.java
@@ -0,0 +1,23 @@
+package com.akram.livres.restController;
+
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.akram.livres.entities.User;
+import com.akram.livres.service.UserService;
+@RestController
+@CrossOrigin(origins = "*")
+public class UserRestController {
+    @Autowired
+    UserService userService;
+    @RequestMapping(path = "all", method = RequestMethod.GET)
+    public List<User> getAllUsers() {
+        return userService.findAllUsers();
+    }
+}
